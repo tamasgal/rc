6dfd135a5a8587bb942265a7d16870b50e80e7dc
Tamas Gal <himself@tamasgal.com>
2017-06-12 13:15:16 +0200

    No OHMYREPL but show version in REPL

--- juliarc.jl
+++ juliarc.jl
@@ -1,2 +1,7 @@
-using OhMyREPL
+#using OhMyREPL
 ENV["JULIA_EDITOR"] = "vim"
+ENV["PYTHON"] = "/Users/tamasgal/.pyenv/versions/3.6.0/bin/python"
+@spawn @sync begin
+sleep(5)
+    Base.active_repl.interface.modes[1].prompt="v$VERSION> "
+end
