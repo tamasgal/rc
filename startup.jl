c50ca11f0e700f4f4514c1c0c5f9ab3d8af12c8c
Tamas Gal <himself@tamasgal.com>
2019-07-11 09:56:09 +0200

    Use fallback when Revise or OhMyREPL are not installed

--- startup.jl
+++ startup.jl
@@ -1,9 +1,16 @@
-using OhMyREPL
+try
+    using OhMyREPL
+catch
+    println("No OhMyREPL installed.")
+end
 
 atreplinit() do repl
     try
         @eval using Revise
         @async Revise.wait_steal_repl_backend()
     catch
+        println("No Revise installed.")
     end
 end
+
+ENV["PYTHON"] = "/home/tgal/.pyenv/shims/python"
