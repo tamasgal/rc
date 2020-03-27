f4f7993958de4731eb1b59b16d57913e33101d1a
Tamas Gal <tgal@km3net.de>
2020-03-27 10:41:32 +0100

    Update doom settings

--- doom.d/init.el
+++ doom.d/init.el
@@ -39,11 +39,11 @@
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup            ; tame sudden yet inevitable temporary windows
-        +all             ; catch all popups that start with an asterix
+        ;;+all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
        ;; (pretty-code       ; replace bits of code with pretty symbols
        ;;  +fira)
-       tabs              ; an tab bar for Emacs
+       ;; tabs              ; an tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
        ;; unicode           ; extended unicode support for various languages
        ;; vc-gutter         ; vcs diff in the fringe
@@ -109,7 +109,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;assembly          ; assembly for fun or debugging
-       ;;cc                ; C/C++/Obj-C madness
+       cc                ; C/C++/Obj-C madness
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
--- doom.d/packages.el
+++ doom.d/packages.el
@@ -50,3 +50,8 @@
 ;; This is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
+
+(use-package! org-re-reveal
+    :after ox
+    :init
+    (setq org-re-reveal-root "https://revealjs.com"))
