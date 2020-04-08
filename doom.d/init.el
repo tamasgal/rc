02fb19ddfb856e7b26836287ed04d33c83c558b0
Tamas Gal <tgal@km3net.de>
2020-04-08 08:48:27 +0200

    Update doom

--- doom.d/init.el
+++ doom.d/init.el
@@ -123,7 +123,7 @@
        ;;ess               ; emacs speaks statistics
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp           ; ML stands for Microsoft's Language
-       ;;go                ; the hipster dialect
+       go                ; the hipster dialect
        ;; (haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
