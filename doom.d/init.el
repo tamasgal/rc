1c2f485c9ef023253f6d0ba74a454a55dfd83d62
Tamas Gal <tgal@km3net.de>
2020-05-18 23:22:23 +0200

    Readd fuzzy

--- doom.d/init.el
+++ doom.d/init.el
@@ -23,7 +23,7 @@
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        (ivy                ; a search engine for love and life
-        ;; +fuzzy
+        +fuzzy
         )
 
        :ui
