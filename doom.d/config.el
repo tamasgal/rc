a303ae67d72d804230b40ed2737abdd4eee54071
Tamas Gal <himself@tamasgal.com>
2020-09-24 11:58:00 +0200

    Cosmetics

--- doom.d/config.el
+++ doom.d/config.el
@@ -18,7 +18,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Iosevka" :size 13))
+(setq doom-font (font-spec :family "Iosevka" :size 12))
 (setq doom-big-font (font-spec :family "Iosevka" :size 26))
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
