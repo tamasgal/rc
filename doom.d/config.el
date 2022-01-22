777c1c93750839e49b8833be2dffcd7d639eab07
Tamas Gal <himself@tamasgal.com>
2022-01-22 10:34:49 +0100

    Add template for ignored files in magit

--- doom.d/config.el
+++ doom.d/config.el
@@ -200,3 +200,17 @@
 
 ;; Org Roam
 (setq org-roam-directory "~/Dropbox/org/roam")
+
+;; Magit
+;; (defun magit-ignored-files ()
+;;   (magit-git-items "ls-files" "--others" "--ignored" "--exclude-standard" "-z" "--directory"))
+
+;; (defun magit-insert-ignored-files ()
+;;   (-when-let (files (magit-ignored-files))
+;;     (magit-insert-section (ignored)
+;;       (magit-insert-heading "Ignored files:")
+;;       (magit-insert-un/tracked-files-1 files nil)
+;;       (insert ?\n))))
+
+;; (after! magit
+;;   (magit-add-section-hook 'magit-insert-ignored-files))
