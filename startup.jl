792b860f867ece2debd58022424a171defe08e89
Tamas Gal <himself@tamasgal.com>
2022-09-20 15:24:30 +0200

    Update julia startup file

--- startup.jl
+++ startup.jl
@@ -1,3 +1,8 @@
+if isfile("Project.toml") && isfile("Manifest.toml")
+    using Pkg
+    Pkg.activate(".")
+end
+
 # try
 #     using OhMyREPL
 # catch
@@ -9,12 +14,11 @@ catch e
     @warn "Error initializing Revise" exception=(e, catch_backtrace())
 end
 
-
-function showsubtypes(type; level = 1, indent = 4)
-    level == 1 && println(type)
-    for st in subtypes(type)
-        print(join(fill(" ", indent * level)))
-        println(st)
-        showsubtypes(st; level = level + 1, indent = indent)
-    end
-end
+#function showsubtypes(type; level = 1, indent = 4)
+#    level == 1 && println(type)
+#    for st in subtypes(type)
+#        print(join(fill(" ", indent * level)))
+#        println(st)
+#        showsubtypes(st; level = level + 1, indent = indent)
+#    end
+#end
