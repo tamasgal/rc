a4dbffdef2362319f6e21c18c44e33eec216544f
Tamas Gal <himself@tamasgal.com>
2023-04-29 08:58:12 +0200

    Switch to vertico

--- doom.d/init.el
+++ doom.d/init.el
@@ -22,10 +22,10 @@
        (company +childframe)           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy                ; a search engine for love and life
-        +fuzzy
-        )
-       ;;vertico           ; the search engine of the future
+       ;; (ivy                ; a search engine for love and life
+       ;;  +fuzzy
+       ;;  )
+       (vertico +fuzzy +marginalia +icons)           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
